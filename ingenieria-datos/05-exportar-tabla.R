441ac8fa91c2b22c503dea19c8f6bf7f982e078a
jgjuara <jg.juara@gmail.com>
2024-10-06 21:32:02 -0300

    exportar tabla como dataset

--- ingenieria-datos/05-exportar-tabla.R
+++ ingenieria-datos/05-exportar-tabla.R
@@ -1,16 +1,29 @@
-library(DBI)
-# Connect to my-db as defined in ~/.my.cnf
-con <- dbConnect(
-  RMySQL::MySQL(),
-  user = Sys.getenv("usersrv"),
-  password = Sys.getenv('password'),
-  dbname = 'competencia_uno',
-  host = Sys.getenv('ip'),
-  port = 3306
-)
+# library(DBI)
+# # Connect to my-db as defined in ~/.my.cnf
+# con <- dbConnect(
+#   RMySQL::MySQL(),
+#   user = Sys.getenv("usersrv"),
+#   password = Sys.getenv('password'),
+#   dbname = 'competencia_uno',
+#   host = Sys.getenv('ip'),
+#   port = 3306
+# )
+# 
+# tabla <- "datasets/competencia_01_aum.parquet"
+# 
+# df <- arrow::read_parquet(glue::glue("{tabla}"))
+# 
+# dbWriteTable(conn = con, value = df, name = "competencia_01_aum")
 
-tabla <- "datasets/competencia_01_aum.parquet"
+library(arrow)
 
-df <- arrow::read_parquet(glue::glue("{tabla}"))
+competencia_01_aum <- open_dataset("datasets/competencia_01_aum.parquet")
 
-dbWriteTable(conn = con, value = df, name = "competencia_01_aum")
+
+tamanio <- file.size("datasets/competencia_01_aum.parquet")
+
+
+competencia_01_aum %>% 
+  group_by(foto_mes) %>% 
+  write_dataset(., path = "datasets/competencia_01_aum/",
+                max_rows_per_file = 42000)
