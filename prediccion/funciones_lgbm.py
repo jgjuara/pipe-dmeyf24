fdbb0d45758d90df2339bb49d438322089e2f140
jgjuara <jg.juara@gmail.com>
2024-10-22 15:34:53 -0300

    reorder print shape

--- prediccion/funciones_lgbm.py
+++ prediccion/funciones_lgbm.py
@@ -53,8 +53,6 @@ def preparar_data(dbname, mes_train, mes_test, drop_cols=None, sampling = 1):
     # Execute the join query and fetch the result as a Pandas DataFrame
     data = con.execute(query).fetchdf()
 
-    print(data.shape)
-
     data = data.drop(['numero_de_cliente_1', 'foto_mes_1', 'clase_ternaria_1'], axis=1)
 
     # data = pd.read_parquet(dataset_path + dataset_file)
@@ -76,6 +74,8 @@ def preparar_data(dbname, mes_train, mes_test, drop_cols=None, sampling = 1):
     data_continua = data_continua.groupby('foto_mes').sample(frac=sampling, random_state=42)
 
     data = pd.concat([data_continua, data[data['clase_ternaria'] != 'CONTINUA']])
+
+    print(data.shape)
 
     print(data.head())
 
