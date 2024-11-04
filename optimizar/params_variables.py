2a73e29bd419509b8070745319440ccf70551233
jgjuara <jg.juara@gmail.com>
2024-11-04 09:12:40 -0300

    sampling

--- optimizar/params_variables.py
+++ optimizar/params_variables.py
@@ -2,5 +2,5 @@ mes_train = [202011,202012,202101,202102,202103, 202104]
 mes_test = [202105]
 n_envios = 10
 intentos = 200
-sampling = 1
-study_name = "sinsampling_basico"
+sampling = 0.5
+study_name = "sampling_basico_50perc"
