7944bb3a854aa1f17cb88b8b56495b089234d24e
jgjuara <jg.juara@gmail.com>
2024-11-06 15:37:47 -0300

    exp104

--- optimizar/params_variables.py
+++ optimizar/params_variables.py
@@ -1,6 +1,6 @@
 mes_train = [202011,202012,202101,202102,202103, 202104]
 mes_test = [202105]
 n_envios = 10
-intentos = 150
-sampling = 1
-study_name = "exp103"
+intentos = 100
+sampling = 0.5
+study_name = "exp104"
