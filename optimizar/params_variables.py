0f05d933142b166f8dd3d329de4a2f771fee4467
jgjuara <jg.juara@gmail.com>
2024-11-07 21:00:14 -0300

    exo106 samp 0.1

--- optimizar/params_variables.py
+++ optimizar/params_variables.py
@@ -1,6 +1,6 @@
 mes_train = [202011,202012,202101,202102,202103, 202104]
 mes_test = [202105]
 n_envios = 10
-intentos = 50
-sampling = 0.25
-study_name = "exp105"
+intentos = 100
+sampling = 0.1
+study_name = "exp106"
